f48927626ccef02fcc7edf1b3973799ce40ecf4f
RJrijo123 <rijojacob2001@gmail.com>
2024-08-20 16:05:31 +0530

    commit

--- chat-app/public/src/App.js
+++ chat-app/public/src/App.js
@@ -4,13 +4,11 @@ import SetAvatar from "./components/SetAvatar";
 import Chat from "./pages/Chat";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Contacts from "./components/Contacts";
 export default function App() {
   return (
     <BrowserRouter basename="/Chat-App">
       <Routes>
-      <Route path="/" element={<Contacts />} />
-      <Route path="/chat" element={<Chat />} />
+      <Route path="/" element={<Chat />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/setAvatar" element={<SetAvatar />} />
