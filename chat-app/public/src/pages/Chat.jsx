b88af33c8b7f581e91efb8e095de7147b9ef86a8
RJrijo123 <rijojacob2001@gmail.com>
2024-08-20 12:37:23 +0530

    commit

--- chat-app/public/src/pages/Chat.jsx
+++ chat-app/public/src/pages/Chat.jsx
@@ -56,8 +56,9 @@ export default function Chat() {
           ) : (
             <ChatContainer currentChat={currentChat} socket={socket} />
           )}
+          <Logout />
         </div>
-        <Logout />
+        
       </Container>
     </>
   );
