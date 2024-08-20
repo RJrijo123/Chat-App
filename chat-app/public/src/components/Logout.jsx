47e1ac43038e35f0a6abf700b45a42bde7120f9b
RJrijo123 <rijojacob2001@gmail.com>
2024-08-20 12:13:05 +0530

    commit

--- chat-app/public/src/components/Logout.jsx
+++ chat-app/public/src/components/Logout.jsx
@@ -2,31 +2,15 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { BiPowerOff } from "react-icons/bi";
 import styled from "styled-components";
-import axios from "axios";
-import { logoutRoute } from "../utils/APIRoutes";
 
 export default function Logout() {
   const navigate = useNavigate();
 
-  const handleClick = async () => {
-    // Use the specific key for retrieving the current user
-    const user = localStorage.getItem("chat-app-current-user");
-
-    if (user) {
-      const { _id } = JSON.parse(user);
-
-      try {
-        const { status } = await axios.get(`${logoutRoute}/${_id}`);
-        
-        if (status === 200) {
-          // Clear localStorage and navigate to login page
-          localStorage.clear();
-          navigate("/login");
-        }
-      } catch (error) {
-        console.error("Error during logout:", error);
-      }
-    }
+  const handleClick = () => {
+    // Clear local storage
+    localStorage.clear();
+    // Redirect to login page
+    navigate("/login");
   };
 
   return (
